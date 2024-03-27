5c2b2238d8ad59eb064deeeffff8485c4cffa7db
Somnath Paul <122199191+paul8478@users.noreply.github.com>
2024-03-27 23:03:40 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is SOMNATH PAUL. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'somnathpaul626@gmail.com',
   phone: '+91 6290600265',
-  address: '84/1/1 Abinash Banerjee Lane, Howrah - 711104, West Bengal. ',
+  address: '84/1/1 Abinash Banerjee Lane, Howrah - 711104, West Bengal, India. ',
   
   github: 'https://github.com/paul8478',
   facebook: '',
